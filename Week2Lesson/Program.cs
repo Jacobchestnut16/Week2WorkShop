78d09981d037c95cc3113081d0c30d0789dda8c1
jchestnut <e16.jmc@gmail.com>
2024-01-16 18:58:22 -0500

    Commit 1

---
+++ Week2Lesson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Week2Lesson
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Car myFirstCar = new Car();
+            //define size small compact car
+            //medium suv
+            //large truck
+            myFirstCar = new Car(2024, "myCarMake", "e219",
+                4, 42999.99, Color.White, Size.Medium);
+            Console.WriteLine(myFirstCar.ToString());
+        }
+    }
+}
